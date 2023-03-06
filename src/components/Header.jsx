0a7a62caac2bc183ad74d6157729009b41681718
Maarten Louage <me@maartenlouage.nl>
2023-03-06 13:40:58 +0100

    Mobile fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 
-import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
-import { DiamondIcon } from '@/components/DiamondIcon'
 
 import Logo from '@/images/Logo.png'
 
@@ -22,7 +20,7 @@ export function Header() {
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-green-600/10 py-4 font-mono text-sm text-green-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
-            <p>Werkspoorkathedraal - Studio B, Utrecht</p>
+            <p>WSK - Studio B, Utrecht</p>
           </div>
         </div>
 
