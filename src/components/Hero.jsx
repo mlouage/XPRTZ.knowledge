03f2b6b15f1064f26f2966a588072557cbe3f035
Maarten Louage <me@maartenlouage.nl>
2023-03-06 14:54:03 +0100

    header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,8 +42,8 @@ export function Hero() {
             {[
               ['Sprekers', '18'],
               // ['People Attending', '2,091'],
-              ['Locatie', 'WSK - Studio B'],
-              ['Plaats', 'Utrecht'],
+              ['Locatie', 'Studio B'],
+              ['Plaats', 'Werkspoorkathedraal, Utrecht'],
             ].map(([name, value]) => (
               <div key={name}>
                 <dt className="font-mono text-sm text-green-600">{name}</dt>
