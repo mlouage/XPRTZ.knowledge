f395c6ffde8abde04980f172e9df491c11a2dbe9
Maarten Louage <mlouage@users.noreply.github.com>
2023-03-25 14:23:21 +0100

    Add send email (#5)
    * Add email handler
    
    * Add subscribe banner
    
    * Add notification banner logic
    
    * Use sendgrid to send emails
    
    * Use more env  vars

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,8 +1,10 @@
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-newsletter.jpg'
+import SuccessBanner from '@/components/SuccessBanner'
 
 function ArrowRightIcon(props) {
   return (
@@ -20,6 +22,40 @@ function ArrowRightIcon(props) {
 }
 
 export function Newsletter() {
+  const [notificationVisible, setNotificationVisible] = useState(false)
+  const [email, setEmail] = useState('')
+  const [honeypot, setHoneypot] = useState('')
+
+  async function handleOnSubmit(e) {
+    e.preventDefault()
+
+    if (honeypot) {
+      // If the honeypot field has a value, it's probably a bot
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/mail', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      })
+
+      if (response.ok) {
+        setNotificationVisible(true)
+        setEmail('')
+      } else {
+        // Handle error
+      }
+    } catch (error) {
+      // Handle error
+    }
+  }
+
+  const handleNotificationClose = () => {
+    setNotificationVisible(false)
+  }
+
   return (
     <section id="newsletter" aria-label="Newsletter">
       <Container>
@@ -38,20 +74,33 @@ export function Newsletter() {
                 Blijf op de hoogte
               </p>
               <p className="mt-4 text-lg tracking-tight text-green-900">
-                Lijkt het je wel wat om een keer langs te komen? Geen probleem, laat je e-mail adres achter en we maken direct een afspraak!
+                Lijkt het je wel wat om een keer langs te komen? Geen probleem,
+                laat je e-mail adres achter en we maken direct een afspraak!
               </p>
             </div>
-            <form>
+            <form method="post" onSubmit={handleOnSubmit}>
               <h3 className="text-lg font-semibold tracking-tight text-green-900">
-                Ik wil op de hoogte blijven <span aria-hidden="true">&darr;</span>
+                Ik wil op de hoogte blijven{' '}
+                <span aria-hidden="true">&darr;</span>
               </h3>
               <div className="mt-5 flex rounded-3xl bg-white py-2.5 pr-2.5 shadow-xl shadow-green-900/5 focus-within:ring-2 focus-within:ring-green-900">
                 <input
+                  id="email"
+                  name="email"
                   type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                   placeholder="E-mail adres"
                   aria-label="E-mail adress"
                   className="-my-2.5 flex-auto bg-transparent pl-6 pr-2.5 text-base text-slate-900 placeholder:text-slate-400 focus:outline-none"
+                />
+                <input
+                  type="text"
+                  name="honeypot"
+                  value={honeypot}
+                  onChange={(e) => setHoneypot(e.target.value)}
+                  style={{ display: 'none' }} // Hide the honeypot field
                 />
                 <Button type="submit">
                   <span className="sr-only sm:not-sr-only">Inschrijven</span>
@@ -60,6 +109,10 @@ export function Newsletter() {
                   </span>
                 </Button>
               </div>
+              <SuccessBanner
+                isVisble={notificationVisible}
+                onClose={handleNotificationClose}
+              />
             </form>
           </div>
         </div>
---
+++ src/pages/api/mail.js
@@ -0,0 +1,25 @@
+import { subscribeToNewsletter } from '../../lib/newsletter';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Email is required' });
+    }
+
+    try {
+      // Call your email service to subscribe the user
+      await subscribeToNewsletter(email);
+
+      return res.status(200).json({ message: 'Subscribed successfully' });
+    } catch (error) {
+      console.error('Error subscribing:', error.message);
+
+      return res.status(500).json({ error: 'Failed to subscribe' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({ error: `Method ${req.method} not allowed` });
+  }
+}
