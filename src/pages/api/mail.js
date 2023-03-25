85a150d4b8884b886454630170e9267f9bf8af88
Maarten Louage <me@maartenlouage.nl>
2023-03-25 13:52:49 +0100

    Add notification banner logic

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -1,14 +1,23 @@
-const handler = async (req, res) => {
-  switch (req.method) {
-    case 'POST':
-      const body = JSON.parse(req.body);
-      const { email } = body;
-      res.json({ email });
-      break;
-    default:
-      res.status(405).end();
-      break;
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Email is required' });
+    }
+
+    try {
+      // Call your email service to subscribe the user
+      
+
+      return res.status(200).json({ message: 'Subscribed successfully' });
+    } catch (error) {
+      console.error('Error subscribing:', error.message);
+
+      return res.status(500).json({ error: 'Failed to subscribe' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({ error: `Method ${req.method} not allowed` });
   }
 }
-
-export default handler;
